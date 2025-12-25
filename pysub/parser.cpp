18b942a0b11be22ae3d9614ea309b39ffb38f7e4
nicklaskuo <kuonicklas@gmail.com>
2025-12-24 18:08:10 -0800

    basic parser structure

--- pysub/parser.cpp
+++ pysub/parser.cpp
@@ -1,9 +1,114 @@
 #include "parser.hpp"
 
+#include <stdexcept>
+
+// return true and consume one token if curr token matches any of the inputs
 bool Parser::Match(auto&&... input) {
-	bool input_matches = (Check(input) && ... && true);
-	if (input_matches) {
+	bool match_found = false;
+	(
+		[&] {
+			if (!match_found) {
+				match_found = Check(input) || match_found;
+			}
+		}() && ...
+	);
+	if (match_found) {
 		IncrementToken();
 	}
-	return input_matches;
+	return match_found;
 }
+
+void Parser::IncrementToken() {
+	if (!IsAtEnd()) {
+		++curr_token;
+	}
+}
+
+bool Parser::Check(std::string_view s) const {
+	if (IsAtEnd()) {
+		return false;
+	}
+	return std::get<std::string>(curr_token->value) == s;
+}
+
+bool Parser::Check(Category category) const {
+	if (IsAtEnd()) {
+		return false;
+	}
+	return curr_token->category == category;
+}
+
+bool Parser::Check(const Token& compare_token) const {
+	if (IsAtEnd()) {
+		return false;
+	}
+	return *curr_token == compare_token;
+}
+
+bool Parser::IsAtEnd() const {
+	return curr_token == std::end(tokens);
+}
+
+std::unique_ptr<AST> Parser::BuildAST() {
+	std::unique_ptr<AST> new_ast = std::make_unique<AST>();
+	if (!IsAtEnd()) {
+		new_ast->statements = GetStatements();
+	}
+	return new_ast;
+}
+
+std::vector<std::unique_ptr<Statement>> Parser::GetStatements() {
+	std::vector<std::unique_ptr<Statement>> statements{};
+	statements.push_back(GetStatement());
+	while (!IsAtEnd()) {
+		statements.push_back(GetStatement());
+	}
+	return statements;
+}
+
+std::unique_ptr<Statement> Parser::GetStatement() {
+	//if (Check("if") || Check("while")) {
+	//	return GetCompoundStatement();
+	//}
+	return GetSimpleStatement();
+}
+
+//CompoundStatement Parser::GetCompoundStatement() {
+//	if (Check("if")) {
+//		return GetIfStatement();
+//	}
+//	return GetWhileStatement();
+//}
+
+std::unique_ptr<Statement> Parser::GetSimpleStatement() {
+	// assignment or expression (assignment omitted)
+	return GetExpression();
+}
+
+std::unique_ptr<Expression> Parser::GetExpression() {
+	auto right = GetConjunction();
+	while (Match(Token{ .value = "or", .category = Category::LogicalOperator })) {
+		dynamic_cast<BinaryExpression*>(right.get()) = Token{ .value = "or", .category = Category::LogicalOperator };
+		right->right = GetExpression();
+	}
+	return right;
+}
+
+std::unique_ptr<Expression> GetConjunction() {
+
+}
+
+//IfStatement Parser::GetIfStatement() {
+//	IfStatement new_if_statement{};
+//	if (!Match("if")) {
+//		throw std::runtime_error("\'if\' expected!");
+//	}
+//	new_if_statement.condition = GetExpression();
+//	if (!Match("")) {
+//
+//	}
+//}
+//
+//WhileStatement Parser::GetWhileStatement() {
+//	
+//}
--- pysub/parser.hpp
+++ pysub/parser.hpp
@@ -4,26 +4,101 @@
 #include "globals.hpp"
 
 #include <vector>
+#include <memory>
 
-// credit Robert Nystrom's "Crafting Interpreters" book for the basic structure:
+//struct Expression {
+//
+//};
+//
+//struct IfStatement {
+//	Expression condition;
+//	std::vector<Statement> block;
+//};
+//
+//struct WhileStatement {
+//
+//};
+//
+//using CompoundStatement = std::variant<IfStatement, WhileStatement>;
+//
+//struct SimpleStatement {
+//
+//};
+//
+//using Statement = std::variant<CompoundStatement, SimpleStatement>;
+
+/*
+	either:
+		-add a bumfuck virtual func (or see if one will be necessary anyways)
+			-we may need to use the visitor pattern from the book anyways, i.e. a visitor class with various functions, and an accept() func for each struct that dispatches to the appropriate func
+		-use variant:
+			-not possible with raw data; only pointers.
+
+	using ParsedType = std::variant<
+		std::unique_ptr<Statement*>,
+		std::unique_ptr<BinaryExpression*>,
+		std::unique_ptr<UnaryExpression*>,
+		std::unique_ptr<Grouping*>,
+		std::unique_ptr<Atom>
+	>;
+*/
+
+struct Statement {
+};
+
+struct Expression : Statement {
+};
+
+struct BinaryExpression : Expression {
+	std::unique_ptr<Expression> left;
+	Token op;
+	std::unique_ptr<Expression> right;
+};
+
+struct UnaryExpression : Expression {
+	std::unique_ptr<Expression> right;
+	Token op;
+};
+
+struct Grouping : Expression {
+	std::unique_ptr<Expression> expression;
+};
+
+struct Atom : Expression {
+	Token value;
+};
+
+struct AST {
+	std::vector<std::unique_ptr<Statement>> statements{};
+};
+
+// credit to Robert Nystrom's "Crafting Interpreters" book for the basic structure:
 // https://craftinginterpreters.com/
 class Parser {
 public:
 	explicit Parser(const std::vector<Token>& _tokens) : tokens(_tokens){
 		curr_token = std::cbegin(tokens);
 	}
+
 private:
 	const std::vector<Token>& tokens;
 	std::vector<Token>::const_iterator curr_token;
 
+	std::unique_ptr<AST> BuildAST();
+	std::vector<std::unique_ptr<Statement>> GetStatements();
+	std::unique_ptr<Statement> GetStatement();
+	//std::unique_ptr<CompoundStatement> GetCompoundStatement();
+	std::unique_ptr<Statement> GetSimpleStatement();
+	std::unique_ptr<Expression> GetExpression();
+	/*IfStatement GetIfStatement();
+	WhileStatement GetWhileStatement();*/
+
 	bool Match(auto&&... input);
-	void IncrementToken() {
-		if (curr_token != std::end(tokens)) {
-			++curr_token;
-		}
-	}
+	void IncrementToken();
 	bool Check(std::string_view s) const;
 	bool Check(Category category) const;
+	bool Check(const Token& compare_token) const;
+	bool IsAtEnd() const;
 };
 
 #endif
